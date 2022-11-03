a5ce6263e1ffe7fd9a83ac350c8268abf49ed6de
Bradley Grainger <bgrainger@gmail.com>
2022-11-03 14:03:08 -0700

    Add simple Faithlife.Build project.

---
+++ Build.cs
@@ -0,0 +1,8 @@
+﻿return BuildRunner.Execute(args, build =>
+{
+	build.Target("test")
+	.Does(() => {
+		var tool = DotNetClassicTool.TryCreateFrom("CentrallyManagedTool.csproj", "NUnit.ConsoleRunner", "nunit3-console.exe") ??
+			throw new BuildException("Can't create DotNetClassicTool for NUnit.ConsoleRunner");
+	});
+});
